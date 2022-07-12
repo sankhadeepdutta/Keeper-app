8ea6e85124afc27b561445f7087add1a0036d336
Sankhadeep Dutta <dutta.shankhadeep@gmail.com>
2022-07-12 19:03:05 +0530

    Updates

---
+++ precache-manifest.1aed722732aa9fcd7743757cc87fb511.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8cf6b522291b6f0334f23463f94ede7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4970b253528d8b4f774",
+    "url": "/static/js/2.22d6a103.chunk.js"
+  },
+  {
+    "revision": "74c700da88b4552e4a01",
+    "url": "/static/js/main.4a53fe36.chunk.js"
+  },
+  {
+    "revision": "31cb9d8849daaa195195",
+    "url": "/static/js/runtime-main.028bd04c.js"
+  }
+]);
